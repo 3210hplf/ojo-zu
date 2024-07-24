44e90b22caa54653dd0c36ee8bf1025e78a50912
3210hplf <3210hplf@gmail.com>
2024-07-24 16:50:35 +0900

    add #17 機能面【ログイン機能】

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
 end
