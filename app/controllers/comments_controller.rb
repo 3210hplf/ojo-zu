d8643f31bac98c9c201be309f6a7442ee7c5daee
3210hplf <3210hplf@gmail.com>
2024-08-17 18:26:47 +0900

    自分のコメントを削除可能に

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,16 @@ class CommentsController < ApplicationController
   def create
     comment = current_user.comments.build(comment_params)
     if comment.save
-      redirect_to post_path(comment.post), success: "コメントしました"
+      redirect_to post_path(comment.post), success: 'コメントしました'
     else
-      redirect_to board_path(comment.post), warning: "コメントに失敗しました"
+      redirect_to post_path(comment.post), warning: 'コメントに失敗しました'
     end
+  end
+
+  def destroy
+    comment = current_user.comments.find(params[:id])
+    comment.destroy!
+    redirect_to post_path(comment.post), success: 'コメントを削除しました'
   end
 
   private
