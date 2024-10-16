de81c932292d4b2a9656de3d2e9c49f1ec6d965a
3210hplf <3210hplf@gmail.com>
2024-10-16 23:02:17 +0900

    ヘッダー等を修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,11 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to new_profile_path, success: "ログインが完了しました"
+      if @user.profile
+        redirect_to posts_path, success: "ログインが完了しました！"
+      else
+        redirect_to new_profile_path, success: "ログインが完了しました！さっそくプロフィールを設定しましょう！"
+      end
     else
       flash.now[:warning] = "メールアドレスまたはパスワードが無効です"
       render :new, status: :unprocessable_entity
